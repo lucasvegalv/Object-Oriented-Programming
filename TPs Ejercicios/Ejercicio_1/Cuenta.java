02b9496641b8968534849c87c4af57aaef6e9698
Lucas <lucasvega02@gmail.com>
2023-08-19 11:29:30 -0300

    Modularize packages

--- TPs Ejercicios/Ejercicio_1/Cuenta.java
+++ TPs Ejercicios/Ejercicio_1/Cuenta.java
@@ -1,51 +1,7 @@
-// Punto 1
-public class Persona {
-
-  // Atributos
-  private String nombre;
-  private String apellido;
-  private int numDocumento;
-
-  // Getters
-  public String getNombre() {
-    return nombre;
-  }
-
-  public String getApellido() {
-    return apellido;
-  }
-
-  public int getnumDocumento() {
-    return numDocumento;
-  }
-
-  // Setters
-  public void setNombre(String nombre) {
-    this.nombre = nombre;
-  }
-
-  public void setApellido(String apellido) {
-    this.apellido = apellido;
-  }
-
-  public void setNumDocumento(int numDocumento) {
-    this.numDocumento = numDocumento;
-  }
-
-  // To String
-  public String toString(){
-    return (nombre + " - " + apellido + " - " + numDocumento);
-  }
-
-  // Constructor
-  public Persona(String nombre, String apellido, int numDocumento) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.numDocumento = numDocumento;
-  }
-}
 
 // Punto 2
+import \Ejercicio_1.Persona;
+
 public class Cuenta {
 
   // Atributos
--- TPs Ejercicios/Ejercicio_1/main.java
+++ TPs Ejercicios/Ejercicio_1/main.java
@@ -2,4 +2,9 @@ package Object-Oriented-Programming.TPs Ejercicios;
 
 public static void main (String[] args) {
   // TO DO
+
+  // Revisar como son los paquetes en VS Code para importar correctamente
+  // Hacer el punto 4
+
+
 }
