2a026d94f7ede3ca7cda0d70de6087e6255ff272
Lucas Vega <lucasvega02@gmail.com>
2023-08-20 16:11:16 -0300

    All exercises done

--- TPs Ejercicios/Ejercicio1/src/main.java
+++ TPs Ejercicios/Ejercicio1/src/main.java
@@ -1,15 +1,5 @@
 import Classes.Persona;
 import Classes.Cuenta;
-
-// 4.Crear un programa (Clase Main) que utilice las clases indicadas y además: 
-//   a.Cree 2 titulares 
-//   b.Cree una cuenta para cada titular. 
-//   c.Cree una cuenta con Titular (sin saldo) y otra con Titular y saldo. 
-//   d.Retire Dinero en la primera cuenta. Muestre el resultado por pantalla 
-//   e.Ingrese Dinero de la segunda cuenta. Muestre el saldo existente por pantalla 
-//   f.Verifique si ambas cuentas tienen el mismo saldo y muéstrelo por pantalla. 
-
-
 public class main {
     public static void main(String[] args) throws Exception {
 
