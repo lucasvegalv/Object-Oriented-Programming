92dc707f948d746dbb1d8589ba7e1191b45899a2
Lucas Vega <lucasvega02@gmail.com>
2023-08-20 15:38:16 -0300

    Transfer, Deposit and Withdraw methods done

--- TPs Ejercicios/Ejercicio1/src/Classes/Cuenta.java
+++ TPs Ejercicios/Ejercicio1/src/Classes/Cuenta.java
@@ -1,12 +1,67 @@
 package Classes;
 import Classes.Persona;
+import java.util.Scanner;
 
 public class Cuenta {
+
+  Scanner scanner = new Scanner(System.in);
 
   // Atributos
   private Persona titular;
   private float saldo;
   private boolean esActivo;
+
+  // Metodo para ingresar dinero a la cuenta
+  public void ingresar (double importe) {
+     if(importe > 0) { // Si ingresa un importe negativo no hacemos nada
+      this.saldo += importe;
+     }
+  }
+
+  // Metodo para retirar dinero de la cuenta
+  
+  public void retirar(double importe) {
+    
+    if(importe > 0){
+      this.saldo -= importe;
+    } else { // Si el importe es igual o menor a cero ofrecemos retirar todo o cancelar el retiro
+      System.out.print("Quiere retirar el saldo existente o cancelar el retiro? R (retirar) - C (cancelar)");
+      String respuesta = scanner.nextLine();
+      
+      while(respuesta != "R" || respuesta != "C") { // Solicitamos respuesta hasta que ingrese lo solicitado
+        System.out.print("Quiere retirar el saldo existente o cancelar el retiro? R (retirar) - C (cancelar)");
+        respuesta = scanner.nextLine();
+      }
+
+      if(respuesta == "R") {
+        this.saldo = 0;
+      } else {
+        System.out.print("Retiro cancelado");
+      }
+    }
+  }
+
+  // Metodo para transferir dinero a otra cuenta
+  public void transferir(Cuenta destino, double importe) {
+    
+    if(importe > this.saldo) {
+      System.out.print("El monto es mayor al saldo disponible. Quiere transferirlo todo o cancelar la operacion? T (transferir) - C (cancelar)");
+      String respuesta = scanner.nextLine();
+
+      while(respuesta != "T" || respuesta != "C") {
+        System.out.print("El monto es mayor al saldo disponible. Quiere trasnferirlo todo o cancelar la operacion? T (transferir) - C (cancelar)");
+        respuesta = scanner.nextLine();
+      }
+
+      if(respuesta == "T") {
+        destino.saldo += this.saldo;
+      } else {
+        System.out.print("Operacion cancelada");
+      }
+
+    }
+
+  }
 
   // Getters
   public Persona getTitular() {
